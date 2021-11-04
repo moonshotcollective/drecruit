88c67e572e6a38304b99b351cb41a1bafaa6b807
qedk <1994constant@gmail.com>
2021-11-04 18:23:18 +0530

    Lint project

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,36 +1,36 @@
-const fastify = require("fastify")({ logger: true });
-const mongoose = require("mongoose");
-const config = require("config");
-const { makeCeramicClient } = require("./helpers/ceramic");
+const fastify = require('fastify')({ logger: true })
+const mongoose = require('mongoose')
+const config = require('config')
+const { makeCeramicClient } = require('./helpers/ceramic')
 
-const { corsOptions, sessionOptions, dbUrl, port } =
-  config.get("API_CONFIG.api");
-fastify.register(require("fastify-cors"), {
-  ...corsOptions,
-});
+const { corsOptions, sessionOptions, dbUrl, port } = config.get('API_CONFIG.api')
 
-fastify.register(require("fastify-secure-session"), {
+fastify.register(require('fastify-cors'), {
+  ...corsOptions
+})
+
+fastify.register(require('fastify-secure-session'), {
   cookieName: sessionOptions.cookieName,
-  key: Buffer.from(sessionOptions.key, "hex"),
+  key: Buffer.from(sessionOptions.key, 'hex'),
   cookie: {
-    ...sessionOptions.cookie,
-  },
-});
+    ...sessionOptions.cookie
+  }
+})
 
-fastify.register(require("./routes"));
+fastify.register(require('./routes'))
 
 // Run the server!
 const start = async () => {
   try {
-    await mongoose.connect(dbUrl);
-    fastify.log.info("DB connected");
-    const client = await makeCeramicClient();
-    fastify.decorate("ceramic", { client });
-    await fastify.listen(port || 5000);
+    await mongoose.connect(dbUrl)
+    fastify.log.info('DB connected')
+    const client = await makeCeramicClient()
+    fastify.decorate('ceramic', { client })
+    await fastify.listen(port || 5000)
   } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
+    fastify.log.error(err)
+    process.exit(1)
   }
-};
+}
 
-start();
+start()
