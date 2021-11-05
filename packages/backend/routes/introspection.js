46e447eb0e5d6dfc535509b46168ceea747af011
qedk <1994constant@gmail.com>
2021-11-05 13:58:09 +0530

    Add endpoints to check auth and URI status

--- packages/backend/routes/introspection.js
+++ packages/backend/routes/introspection.js
@@ -1,3 +1,5 @@
+const { getDidFromTokenURI, selfIdCore } = require('../helpers/ceramic')
+
 module.exports = function (fastify, opts, done) {
   fastify.get('/', async (request, reply) => {
     return { statusCode: 200, message: 'dRecruit API' }
@@ -5,6 +7,22 @@ module.exports = function (fastify, opts, done) {
 
   fastify.get('/did', async (request, reply) => {
     return fastify.ceramic.client.did.id
+  })
+
+  fastify.get('/verify/:tokenId', async (request, reply) => {
+    try {
+      const { did } = getDidFromTokenURI(request.params.tokenId)
+      const privateProfile = await selfIdCore.get('privateProfile', did)
+      try {
+        await fastify.ceramic.client.did.decryptDagJWE(JSON.parse(privateProfile.encrypted))
+      } catch (err) {
+        return { statusCode: 400 }
+      }
+      return { statusCode: 200 }
+    } catch (err) {
+      fastify.log.error(err)
+      return { statusCode: 500 }
+    }
   })
 
   done()
