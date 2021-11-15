3c2df9d13eeb5a24627873806f258baa63d52e46
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-11-15 14:58:14 +0530

    fix: Remove support btn & faucet UI

--- packages/react-app/components/DevUI.jsx
+++ packages/react-app/components/DevUI.jsx
@@ -15,42 +15,6 @@ function DevUI({ web3 }) {
       </div>
 
       <ThemeSwitch />
-
-      {/* 🗺 Extra UI like gas price, eth price, faucet, and support: */}
-      <div style={{ position: "fixed", textAlign: "left", left: 0, bottom: 8, padding: 10 }}>
-        <Row align="middle" gutter={[4, 4]}>
-          <Col span={8}>
-            <Ramp {...web3} networks={NETWORKS} />
-          </Col>
-
-          <Col span={8} style={{ textAlign: "center", opacity: 0.8 }}>
-            <GasGauge {...web3} />
-          </Col>
-          <Col span={8} style={{ textAlign: "center", opacity: 1 }}>
-            <Button
-              onClick={() => {
-                window.open("https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA");
-              }}
-              size="large"
-              shape="round"
-            >
-              <span style={{ marginRight: 8 }} role="img" aria-label="support">
-                💬
-              </span>
-              Support
-            </Button>
-          </Col>
-        </Row>
-
-        <Row align="middle" style={{ marginTop: 10 }} gutter={[4, 4]}>
-          <Col span={24}>
-            {
-              /*  if the local provider has a signer, let's show the faucet:  */
-              web3.faucetAvailable ? <Faucet {...web3} ensProvider={web3.mainnetProvider} /> : ""
-            }
-          </Col>
-        </Row>
-      </div>
     </>
   );
 }
