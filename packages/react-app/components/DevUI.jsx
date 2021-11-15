67803d01bb5a014a503611af2bab23c1f47110b7
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-11-16 00:13:43 +0530

    fix: increase account/wallet display z-index

--- packages/react-app/components/DevUI.jsx
+++ packages/react-app/components/DevUI.jsx
@@ -9,7 +9,7 @@ function DevUI({ web3 }) {
     <>
       {web3.networkDisplay}
       {/* 👨‍💼 Your account is in the top right with a wallet at connect options */}
-      <div style={{ position: "fixed", textAlign: "right", right: 0, top: 0, padding: 10 }}>
+      <div style={{ position: "fixed", textAlign: "right", right: 0, top: 0, padding: 10, zIndex: 2000 }}>
         <Account {...web3} />
         {web3.faucetHint}
       </div>
