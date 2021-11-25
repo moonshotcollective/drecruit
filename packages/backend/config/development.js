44fb1c21f7cb6808b0f60924e5f47ba524ad6fb3
Cali93 <armut.cumaali@gmail.com>
2021-11-25 10:31:49 +0100

    logout - delete cookie

--- packages/backend/config/development.js
+++ packages/backend/config/development.js
@@ -1,34 +1,33 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   API_CONFIG: {
     api: {
       sessionOptions: {
-        cookieName: process.env.COOKIE_NAME || 'drecruit-session-dev',
+        cookieName: process.env.COOKIE_NAME || "drecruit-session-dev",
         key: process.env.COOKIE_KEY,
         cookie: {
           // options for setCookie, see https://github.com/fastify/fastify-cookie
-          domain: process.env.DOMAIN,
-          secure: true,
+          secure: false,
           httpOnly: true,
-          path: '/',
-          maxAge: 144 * 60 * 60 * 1000 // 6 days
-        }
+          path: "/",
+          maxAge: 144 * 60 * 60 * 1000, // 6 days
+        },
       },
       corsOptions: {
         credentials: true,
         origin: function (origin, callback) {
           const validPatternRegexes = [
             /^(.*)staging.recruiter.party(\/(.*)|)$/,
-            /^http:\/\/localhost:[0-9]{4}$/
-          ]
+            /^http:\/\/localhost:[0-9]{4}$/,
+          ];
           if (validPatternRegexes.some((rx) => rx.test(origin)) || !origin) {
-            callback(null, true)
+            callback(null, true);
           } else {
-            callback(new Error('Not allowed by CORS'))
+            callback(new Error("Not allowed by CORS"));
           }
-        }
-      }
-    }
-  }
-}
+        },
+      },
+    },
+  },
+};
--- packages/backend/config/production.js
+++ packages/backend/config/production.js
@@ -1,20 +1,21 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   API_CONFIG: {
     api: {
       sessionOptions: {
-        cookieName: process.env.COOKIE_NAME || 'drecruit-session',
+        cookieName: process.env.COOKIE_NAME || "drecruit-session",
         key: process.env.COOKIE_KEY,
         cookie: {
           // options for setCookie, see https://github.com/fastify/fastify-cookie
           domain: process.env.DOMAIN,
           secure: true,
+          sameSite: "None",
           httpOnly: true,
-          path: '/',
-          maxAge: 144 * 60 * 60 * 1000 // 6 days
-        }
-      }
-    }
-  }
-}
+          path: "/",
+          maxAge: 144 * 60 * 60 * 1000, // 6 days
+        },
+      },
+    },
+  },
+};
